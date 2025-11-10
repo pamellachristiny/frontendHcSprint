eae55fb68c898fd536151721c2bce5839ddeafdf
pamellachristiny <pamella.christiny2006@gmail.com>
2025-11-09 22:00:12 -0300

    fix: export default adicionado na página Integrantes

--- src/pages/Integrantes.tsx
+++ src/pages/Integrantes.tsx
@@ -1,11 +1,7 @@
 function Integrantes() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold text-center mb-4">Integrantes</h1>
-      <ul className="list-disc list-inside space-y-2 text-lg">
-        <li>Pamella Christiny</li>
-        <li>[Outros integrantes aqui]</li>
-      </ul>
+    <div>
+      <h1>Integrantes</h1>
     </div>
   );
 }
