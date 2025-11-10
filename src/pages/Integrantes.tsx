52a142947dd77c2a84c9b8e2be455fd92dc09bf3
pamellachristiny <pamella.christiny2006@gmail.com>
2025-11-09 21:58:40 -0300

    fix: corrigido import da página Integrantes

---
+++ src/pages/Integrantes.tsx
@@ -0,0 +1,13 @@
+function Integrantes() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-center mb-4">Integrantes</h1>
+      <ul className="list-disc list-inside space-y-2 text-lg">
+        <li>Pamella Christiny</li>
+        <li>[Outros integrantes aqui]</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Integrantes;
