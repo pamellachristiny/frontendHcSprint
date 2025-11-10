b964974a09ac7afd3427051c515f1db6717610b9
pamellachristiny <pamella.christiny2006@gmail.com>
2025-11-09 22:16:38 -0300

    fix: export default Integrantes corrigido

--- src/pages/Integrantes.tsx
+++ src/pages/Integrantes.tsx
@@ -1,7 +1,10 @@
+import React from "react";
+
 function Integrantes() {
   return (
-    <div>
-      <h1>Integrantes</h1>
+    <div className="p-8 text-center">
+      <h1 className="text-2xl font-bold">Integrantes do Projeto</h1>
+      <p className="mt-4">Pamella Christiny • [outros nomes]</p>
     </div>
   );
 }
